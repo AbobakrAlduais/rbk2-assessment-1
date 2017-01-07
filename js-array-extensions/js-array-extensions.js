0877e6da29963c97014965448a22dd34007cc505
AbobakrAlduais <aalduais@gmail.com>
2017-01-07 10:31:56 +0200

    Q 3

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -4,3 +4,16 @@ function first (array){
 function last(array){
 	return array[array.length-1]
 }
+
+
+var ArrEX=function(array){
+	return{
+		first:function  (array){
+	return this.array[0]
+},
+
+last:function (array){
+	return this.array[this.array.length-1]
+}
+	}
+}
